e599ea687aafd7d43cd0d9059c218007177f50a5
Linus Torvalds <torvalds@linux-foundation.org>
2022-04-03 18:08:37 +0200

    Move vertex declaration to cpp file

--- src/renderer/vertex.cpp
+++ src/renderer/vertex.cpp
@@ -36,3 +36,30 @@ void Renderer::createVertexBuffer(Renderer::Context *ctx) {
     memcpy(data, vertices.data(), (size_t) bufferInfo.size);
     vkUnmapMemory(ctx->device, ctx->vertexBufferMemory);
 }
+
+VkVertexInputBindingDescription Renderer::Vertex::getBindingDescription() {
+    VkVertexInputBindingDescription bindingDescription{};
+    bindingDescription.binding = 0;
+    bindingDescription.stride = sizeof(Vertex);
+    bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+
+    return bindingDescription;
+}
+
+std::array<VkVertexInputAttributeDescription, 2> Renderer::Vertex::getAttributeDescriptions() {
+    std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions{};
+
+    // Position types
+    attributeDescriptions[0].binding = 0;
+    attributeDescriptions[0].location = 0;
+    attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
+    attributeDescriptions[0].offset = offsetof(Vertex, pos);
+
+    // Color types
+    attributeDescriptions[1].binding = 0;
+    attributeDescriptions[1].location = 1;
+    attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+    attributeDescriptions[1].offset = offsetof(Vertex, color);
+
+    return attributeDescriptions;
+}
--- src/renderer/vertex.h
+++ src/renderer/vertex.h
@@ -11,32 +11,9 @@ namespace Renderer {
         glm::vec2 pos;
         glm::vec3 color;
 
-        static VkVertexInputBindingDescription getBindingDescription() {
-            VkVertexInputBindingDescription bindingDescription{};
-            bindingDescription.binding = 0;
-            bindingDescription.stride = sizeof(Vertex);
-            bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+        static VkVertexInputBindingDescription getBindingDescription();
 
-            return bindingDescription;
-        }
-
-        static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions() {
-            std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions{};
-
-            // Position types
-            attributeDescriptions[0].binding = 0;
-            attributeDescriptions[0].location = 0;
-            attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
-            attributeDescriptions[0].offset = offsetof(Vertex, pos);
-
-            // Color types
-            attributeDescriptions[1].binding = 0;
-            attributeDescriptions[1].location = 1;
-            attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
-            attributeDescriptions[1].offset = offsetof(Vertex, color);
-
-            return attributeDescriptions;
-        }
+        static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions();
     };
 
     void createVertexBuffer(Context *ctx);
