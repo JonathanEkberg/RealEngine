5afd57e1c8db25025ba6bdd6422df561e613a432
Jonathan Ekberg <jodrim@gmail.com>
2022-04-06 20:45:37 +0200

    Use environment variable to find shader compiler

--- compile_shader.bat
+++ compile_shader.bat
@@ -1,2 +1,2 @@
-E:\dev\tools\Vulkan\Bin\glslc.exe shaders\shader.vert -o shaders\vert.spv
-E:\dev\tools\Vulkan\Bin\glslc.exe shaders\shader.frag -o shaders\frag.spv
+%VULKAN_SDK%\Bin\glslc.exe shaders\shader.vert -o shaders\vert.spv
+%VULKAN_SDK%\Bin\glslc.exe shaders\shader.frag -o shaders\frag.spv
